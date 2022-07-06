90511917d4ba71b84ff17515aafa855796696b73
Alex Schaffer <alexschaffer@gmail.com>
2022-07-06 17:22:08 -0400

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,9 @@
-var Sent1 = ("\"This the beginning of the string")
-var Sent2 = (" and this is the end of the string\”")
+var Sent1 = ("\"This the beginning of the string") //Created Variable 1
+var Sent2 = (" and this is the end of the string\”") //Created Variable 2
 
-document.write(Sent1 + Sent2); 
+document.write(Sent1 + Sent2); //Concatenating my two variables together
 
-window.alert(5 + 5);
+window.alert(5 + 5); //Adding a Windows alert that 5+5 = 10
 
 
 
