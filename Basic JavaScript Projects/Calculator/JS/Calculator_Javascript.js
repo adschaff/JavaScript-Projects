49dc358df72c16825ef7211a0f1ef3facac748ed
Alex Schaffer <alexschaffer@gmail.com>
2022-07-25 13:21:53 -0400

    fixed_calculator

--- Basic JavaScript Projects/Calculator/JS/Calculator_Javascript.js
+++ Basic JavaScript Projects/Calculator/JS/Calculator_Javascript.js
@@ -51,7 +51,7 @@ function Handle_Operator(Next_Operator) {
         return;
     }
     if (First_Operand == null) {
-        Calculator.First_Operand - Value_of_Input;
+        Calculator.First_Operand = Value_of_Input;
     } else if (operator) {//Checks if an operator already exits
         const Value_Now = First_Operand || 0;
         //if operator exists, property lookup is performer for the operator
